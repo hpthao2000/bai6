25ddc9bb1636762043fb81f3bfd7cb5da28f8add
hpthao2000 <75359722+hpthao2000@users.noreply.github.com>
2020-12-02 04:18:39 -0400

    phiên bản đầu tiên
    chỉ mới đặt tên form

---
+++ CIS2225_Assingment6_Ho_Thao/GuestInformation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS2225_Assingment6_Ho_Thao
+{
+    public partial class GuestInformation : Form
+    {
+        public GuestInformation()
+        {
+            InitializeComponent();
+        }
+    }
+}
